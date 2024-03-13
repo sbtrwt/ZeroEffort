d146339f6a24093cc49c461306fe791b87a8ba5a
Vastav <vastavpansuriya4444@gmail.com>
2024-03-13 17:51:50 +0530

    Input System Package

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,20 +1,13 @@
 ﻿using FPSShooter.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using UnityEngine;
 
 namespace FPSShooter.Player
 {
     public abstract class Player : IMovable
     {
-        private Vector3 position;
         public virtual void OnMove()
         {
 
         }
-       
+
     }
 }
