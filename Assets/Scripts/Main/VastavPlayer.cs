ca92ebb9d219366da70026ef74b0788713a02df2
Vastav <vastavpansuriya4444@gmail.com>
2024-03-13 18:50:59 +0530

    Movement Prototype

---
+++ Assets/Scripts/Main/VastavPlayer.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class VastavPlayer : MonoBehaviour
+{
+    [SerializeField] private float speed;
+
+    private void OnEnable()
+    {
+        VastavPlayerInput.OnMove += VastavPlayerInput_OnMove;
+    }
+
+    private void VastavPlayerInput_OnMove(Vector2 vector)
+    {
+        transform.position += new Vector3(vector.x, 0, vector.y) * Time.deltaTime * speed;
+    }
+
+    private void OnDisable()
+    {
+        VastavPlayerInput.OnMove -= VastavPlayerInput_OnMove;
+    }
+}
---
+++ Assets/Scripts/Main/VastavPlayerInput.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class VastavPlayerInput : MonoBehaviour
+{
+    private PlayerInputAction playerInputActions;
+
+    public static Action<Vector2> OnMove;
+
+    private void Awake()
+    {
+        if (playerInputActions == null)
+        {
+            playerInputActions = new PlayerInputAction();
+            playerInputActions.Enable();
+        }
+    }
+
+    private void Update()
+    {
+        Vector2 moveData = playerInputActions.Player.Movement.ReadValue<Vector2>();
+        OnMove?.Invoke(moveData);
+
+
+        Vector2 mouseDelta = Mouse.current.delta.ReadValue();
+        Debug.Log("Mouse Delta: " + mouseDelta);
+    }
+}
+
