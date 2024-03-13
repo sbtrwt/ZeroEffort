58d086485d3444c303833320f43ac30192e09ad0
shobhit-outscal <shobhit.rawat@outscal.com>
2024-03-13 16:55:32 +0530

    Added IMovable interface

---
+++ Assets/Scripts/Interfaces/IMovable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FPSShooter.Interfaces
+{
+    public interface IMovable
+    {
+        void Move();
+    }
+}
--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FPSShooter.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,12 @@ using System.Threading.Tasks;
 
 namespace FPSShooter.Player
 {
-    public class Player
+    public abstract class Player : IMovable
     {
+        public virtual void Move()
+        {
+
+        }
+       
     }
 }
